b9bc519e4854e24cdf257ffc9155ad006450ffbc
balaknightfang <46404499+balaknightfang@users.noreply.github.com>
2024-03-06 22:19:00 -0500

    tank role now properly accounted for

--- Projects/ChampionsEHPCalculator/main.js
+++ Projects/ChampionsEHPCalculator/main.js
@@ -1,13 +1,14 @@
 const armorPrint = () => {
     let powerNum = Number(document.getElementById("armorValue").value);
     let brill = +(document.getElementById('brill').checked);
+    let tank = +(document.getElementById('tank').checked);
     let pss = Number(document.getElementById("PSS").value);
     let sss = Number(document.getElementById("SSSS").value);
     let inv = +(document.getElementById('invuln').checked)*(39 + .0495 * pss + .046 * sss);
     let def = +(document.getElementById('defiance').checked)*(11.34 + .0188 * pss + .0182 * sss)*6;
-    let r = (100/(100 + powerNum*.2353 + 100*.2*brill+inv+def))*100;
+    let r = (100/(100 + powerNum*.2353 + 100*.2*brill+100*.1*tank+inv+def))*100;
     console.log(r);
-    let res = (100-(100/(100 + powerNum*.2353 + 100*.2*brill+inv+def))*100).toFixed(1);
+    let res = (100-(100/(100 + powerNum*.2353 + 100*.2*brill+100*.1*tank+inv+def))*100).toFixed(1);
     let macro = `Actual Percent Resisted: ${res}%`;
 
     let hpNum = Number(document.getElementById("baseHP").value);
@@ -21,9 +22,10 @@ const armorPrint = () => {
 const resPrint = () => {
     let powerNum = Number(document.getElementById("armorValue2").value);
     let brill = +(document.getElementById('brill2').checked);
+    let tank = +(document.getElementById('tank2').checked);
     let avoid = Number(document.getElementById('avoid').value);
-    let r = (100/(100 + powerNum*.2353 + 100*.2*brill))*((100-avoid));
-    let res = ((100-(100/(100 + powerNum*.2353 + 100*.2*brill))*((100-avoid)))).toFixed(1)
+    let r = (100/(100 + powerNum*.2353 + 100*.2*brill+100*.1*tank))*((100-avoid));
+    let res = ((100-(100/(100 + powerNum*.2353 + 100*.2*brill+.1*tank))*((100-avoid)))).toFixed(1)
     let macro = `Actual Percent Resisted: ${res}%`;
 
     let hpNum = Number(document.getElementById("baseHP2").value);
