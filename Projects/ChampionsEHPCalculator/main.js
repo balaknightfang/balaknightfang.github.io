e8b0fb57a2c013c92162219bf5457a3f4d8d954d
balaknightfang <46404499+balaknightfang@users.noreply.github.com>
2024-05-15 21:28:48 -0400

    blocking fixes and shit

--- Projects/ChampionsEHPCalculator/main.js
+++ Projects/ChampionsEHPCalculator/main.js
@@ -6,15 +6,17 @@ const armorPrint = () => {
     let sss = Number(document.getElementById("SSSS").value);
     let inv = +(document.getElementById('invuln').checked)*(39 + .0495 * pss + .046 * sss);
     let def = +(document.getElementById('defiance').checked)*(11.34 + .0188 * pss + .0182 * sss)*6;
-    let r = (100/(100 + powerNum*.2353 + 100*.2*brill+100*.1*tank+inv+def))*100;
+    let r = (100/(100 + powerNum*.2353 + 100*.2*brill+100*.1*tank+inv+def));
     console.log(r);
     let res = (100-(100/(100 + powerNum*.2353 + 100*.2*brill+100*.1*tank+inv+def))*100).toFixed(1);
-    let macro = `Actual Percent Resisted: ${res}%`;
+    let macro = `Actual Percent Resisted: ${res}%`; 
 
     let hpNum = Number(document.getElementById("baseHP").value);
-    let ehp = `Effective HP: ${( hpNum/(r/100)).toFixed(1)}`;
+    let ehp = `Effective HP: ${( hpNum/(r)).toFixed(1)}`;
+    let blockEHP = `Block EHP:  ${( hpNum/((100/(100 + powerNum*.2353 + 100*.2*brill+100*.1*tank+inv+def+360)))).toFixed(1)}`
 
-    document.getElementById("tankEHP").innerHTML = macro + "<br>" + ehp;
+
+    document.getElementById("tankEHP").innerHTML = macro + "<br>" + ehp + "<br>" + blockEHP;
 
     
 }
@@ -30,8 +32,9 @@ const resPrint = () => {
 
     let hpNum = Number(document.getElementById("baseHP2").value);
     let ehp = `Effective HP: ${(hpNum/(r/100)).toFixed(1)}`;
+    let blockEHP2 = `Block EHP:  ${(hpNum/((100/(100 + powerNum*.2353 + 100*.2*brill+100*.1*tank+360))*((100-avoid))/100)).toFixed(1)}`
 
-    document.getElementById("dodgeEHP").innerHTML = macro + "<br>" + ehp;
+    document.getElementById("dodgeEHP").innerHTML = macro + "<br>" + ehp+ "<br>" + blockEHP2;
 
 }
 
