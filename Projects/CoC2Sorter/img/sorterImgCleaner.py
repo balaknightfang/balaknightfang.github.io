0f2e342f9c0a3e304c7b9f1d2c04e4bcc687e414
balaknightfang <46404499+balaknightfang@users.noreply.github.com>
2024-10-04 23:03:14 -0400

    whoopsy

--- Projects/CoC2Sorter/img/sorterImgCleaner.py
+++
@@ -1,9 +0,0 @@
-import os
-
-directory = "C:\\Users\\Robert\\Documents\\GitHub\\balaknightfang.github.io\\Projects\\CoC2Sorter\\img"
-
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    filenameParts = filename.split(".")
-    newName = filenameParts[0] + "." + filenameParts[-1];
-    os.rename(filename, newName)
